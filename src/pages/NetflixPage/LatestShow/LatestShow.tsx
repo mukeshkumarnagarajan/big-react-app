75d6d6023c005c75e5fefe6555b09c49d19ddf6d
Mukeshkumar Nagarajan <Mukeshkumar.Nagarajan@genesys.com>
2024-03-21 17:03:07 +0530

    nthing much

--- src/pages/NetflixPage/LatestShow/LatestShow.tsx
+++ src/pages/NetflixPage/LatestShow/LatestShow.tsx
@@ -10,14 +10,17 @@ const LatestShow: React.FC<LatestShowProps> = (props) => {
   return (
     <div className="card">
       <img src={props.imgUrl} className="card-img-top" alt={props.title} />
+
       <div className="card-body">
         <h5 className="card-title">{props.title}</h5>
         <p className="card-text">{props.children}</p>
       </div>
+
       <ul className="list-group list-group-flush">
         <li className="list-group-item">{props.category}</li>
         <li className="list-group-item">Published {props.publishedOn}</li>
       </ul>
+
       <div className="card-body">
         <a href="#" className="btn btn-primary">
           Watch Now
