6e6f26ed5e9a7059cc10c65529b3a891e0d823a5
Mukeshkumar Nagarajan <Mukeshkumar.Nagarajan@genesys.com>
2024-03-21 18:01:40 +0530

    Used Hook and State

--- src/pages/NetflixPage/TrendingShow.tsx
+++ src/pages/NetflixPage/TrendingShow.tsx
@@ -1,5 +1,30 @@
-const TrendingShow = () => {
-  return <div></div>;
+interface TrendingShowProps {
+    title: string;
+    imgUrl: string;
+    children: string;
+    category: string;
+    publishedOn: string;
+  }
+
+const TrendingShow:React.FC<TrendingShowProps> = (props) => {
+  return (
+    <div className="card">
+      <img src={props.imgUrl} className="card-img-top" alt={props.title} />
+      <div className="card-body">
+        <h5 className="card-title">{props.title}</h5>
+        <p className="card-text">{props.children}</p>
+      </div>
+      <ul className="list-group list-group-flush">
+        <li className="list-group-item">{props.category}</li>
+        <li className="list-group-item">Published {props.publishedOn}</li>
+      </ul>
+      <div className="card-body">
+        <a href="#" className="btn btn-primary">
+          Watch Now
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default TrendingShow;
--- src/pages/NetflixPage/TrendingShowList.tsx
+++ src/pages/NetflixPage/TrendingShowList.tsx
@@ -1,21 +1,30 @@
-import TrendingShow from "./TrendingShow";
+import { useState } from "react";
+// import TrendingShow from "./TrendingShow";
 
 const TrendingShowList: React.FC = () => {
+  console.log("executing");
+  const [resolution, setResolution] = useState("HD");
+
+  const handleSwitchResolution = () => {
+    setResolution("4K");
+  };
+
   return (
     <div>
       <div className="row">
-        <div className="col-md-3">
-          <TrendingShow />
-        </div>
-        <div className="col-md-3">
-          <TrendingShow />
-        </div>
-        <div className="col-md-3">
-          <TrendingShow />
-        </div>
-        <div className="col-md-3">
-          <TrendingShow />
-        </div>
+        <h3>
+          Enjoy watching the shows in {resolution} | {""}
+          <button
+            className="btn btn-sm btn-warning"
+            onClick={handleSwitchResolution}
+          >
+            Switch to 4K
+          </button>
+        </h3>
+        <div className="col-md-3">{/* <TrendingShow /> */}</div>
+        <div className="col-md-3">{/* <TrendingShow /> */}</div>
+        <div className="col-md-3">{/* <TrendingShow /> */}</div>
+        <div className="col-md-3">{/* <TrendingShow /> */}</div>
       </div>
     </div>
   );
